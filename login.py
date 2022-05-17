6f7189e62d0873268c8b572425c65ca30bd0a0d4
lukachikiri <fatihkaganusta53@gmail.com>
2022-05-17 21:21:21 +0300

    Add files via upload

---
+++ login.py
@@ -0,0 +1,12 @@
+from ast import And
+from operator import and_
+
+
+kayitliKullanici="admin"
+kayitliSifre="123456"
+kullaniciAdi=input("Kullanıcı Adınızı Giriniz: ")
+sifre=input("Şifrenizi Giriniz: ")
+if(kayitliKullanici==kullaniciAdi and kayitliSifre==sifre): 
+    print("Hoşgeldin %s!"%(kullaniciAdi))
+else:   
+    print("Kullanıcı Adı veya Şifre Hatalı!")
